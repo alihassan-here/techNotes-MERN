832bb2ba9acc793796a8324be81d38d994df4e25
alihassa-here <alihassanuog50@gmail.com>
2022-10-08 19:49:37 +0500

    implement jwt in backend

--- backend/routes/noteRoute.js
+++ backend/routes/noteRoute.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const notesController = require('../controllers/noteController')
+const notesController = require('../controllers/noteController');
+const verfiyJWT = require("../middleware/verifyJWT");
+
+
+router.use(verfiyJWT);
 
 router.route('/')
     .get(notesController.getAllNotes)
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { getAllUsers, createUser, updateUser, deleteUser } = require('../controllers/userController');
+const verfiyJWT = require("../middleware/verifyJWT");
+
+
+router.use(verfiyJWT);
 
 
 router.route('/')
