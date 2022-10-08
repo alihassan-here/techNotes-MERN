4bace860edb85c2482075436439ed5ece5e7c364
alihassa-here <alihassanuog50@gmail.com>
2022-10-08 18:46:53 +0500

    user and note crud completed

--- frontend/src/features/users/UsersList.js
+++ frontend/src/features/users/UsersList.js
@@ -7,7 +7,11 @@ const UsersList = () => {
         isSuccess,
         isError,
         error
-    } = useGetUsersQuery();
+    } = useGetUsersQuery(undefined, {
+        pollingInterval: 60000,
+        refetchOnFocus: true,
+        refetchOnMountOrArgChange: true
+    });
     let content;
     if (isLoading) content = <p>Loading...</p>
     if (isError) content = <p className="errmsg">{error?.data?.message}</p>
